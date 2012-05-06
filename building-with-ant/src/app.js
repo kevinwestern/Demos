dda1f495681b2d0932b2bbd912ae50ac0cbd3fc2
Kevin Western <kevin_western25@yahoo.com>
2012-05-06 14:09:07 -0700

    quick test of building with ant

---
+++ building-with-ant/src/app.js
@@ -0,0 +1,39 @@
+var TestBuildOne;
+(function(global) {
+	"use strict";
+	global = {};
+	global.Utils = {
+
+		hasOwn: function (obj, prop) {
+			if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+				return true;
+			}
+
+			return false;
+		},
+
+		namespace: function (path, expr) {
+			var namespaces = path.split('.'),
+				i = 0,
+				len = namespaces.length,
+				currentLevel = global;
+
+			while (i < len) {
+				if (i === len - 1) {
+					currentLevel[namespaces[i]] = expr;
+					break;
+				}
+				else if (!TestBuildOne.Utils.hasOwn(currentLevel, namespaces[i])) {
+					currentLevel = currentLevel[namespaces[i]] = {};
+				}
+				i++;
+			}
+		}
+	};
+
+	global.Lib = {};
+	global.Models = {};
+
+	return global;
+	
+})(TestBuildOne);
---
+++ building-with-ant/src/lib/Class.js
@@ -0,0 +1,7 @@
+/*global TestBuildOne*/
+(function(){
+	"use strict";
+	TestBuildOne.Utils.namespace('TestBuildOne.Lib.Class', function () {
+		return "This is a Class!";
+	});
+})();
